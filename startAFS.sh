2fa9076833a9a9e8ab874ca99806ec0b1d8f214c
Yadavendra Yadav <yadayada@in.ibm.com>
2020-06-07 20:00:53 +0000

    ten

--- startAFS.sh
+++ startAFS.sh
@@ -4,4 +4,7 @@ insmod /build/openafs-1.8.5/src/libafs/MODLOAD-4.18.0-147.8.1.el8_1.x86_64-SP/op
 if [ ! -d "/openafs/afscache" ]; then
 	mkdir /openafs/afscache
 fi
+if [ ! -d "/openafs/afs" ]; then
+	mkdir /openafs/afs
+fi
 /build/openafs-1.8.5/src/afsd/afsd -stat 100000 -daemons 12 -volumes 1536 -chunksize 20 -files 115000 -dcache 25000 -disable-dynamic-vcaches --mountdir=/openafs/afs
