e6f6c3f3e1d3e29fd3cbfae0dab40b8d8b3d5052
Yadavendra Yadav <yadayada@in.ibm.com>
2020-06-09 15:20:07 +0000

    new

--- startStopAFS.sh
+++ startStopAFS.sh
@@ -6,26 +6,31 @@ function usage() {
 }
 
 operation=""
+kversion=""
 
-while getopts "o:" c; do
+while getopts "o:v:" c; do
 
 	case $c in 
 		o) operation=$OPTARG;;
+	   	v) kversion=$OPTARG;;
 	esac			
 done
 
 [ -z $operation ] && usage
+[ -z $kversion ] && usage
 
 
 if [ $operation = "start" ]; then
 
-	insmod /build/openafs-1.8.5/src/libafs/MODLOAD-4.18.0-147.8.1.el8_1.x86_64-SP/openafs.ko
+	cp /build/openafs-1.8.5/src/libafs/MODLOAD-4.18.0-147.8.1.el8_1.x86_64-SP/openafs.ko /lib/modules/${kversion}/
+	depmod -a
 	if [ ! -d "/openafs/afscache" ]; then
 		mkdir /openafs/afscache
 	fi
 	if [ ! -d "/openafs/afs" ]; then
 		mkdir /openafs/afs
 	fi
+	modprobe openafs
 	/build/openafs-1.8.5/src/afsd/afsd -stat 100000 -daemons 12 -volumes 1536 -chunksize 20 -files 115000 -dcache 25000 -disable-dynamic-vcaches --mountdir=/openafs/afs
 elif [ $operation = "stop" ]; then
 	umount /openafs/afs
